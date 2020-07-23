fdd9b0bfdc5987ba999e86fef211608ce97667d6
Negronis <57703160+Negronis@users.noreply.github.com>
2020-07-23 22:40:52 +0800

    首次迭代
    二次更新

--- scrollnew.js
+++ scrollnew.js
@@ -1,20 +1,47 @@
-let divs = document.querySelector('#container').querySelectorAll('div');
-function ScrollDiv(div) {
+// 函数节流
+let throttle = function(func,...args){
+    let timer = null;
+    return function(){
+        if(timer){
+            return;
+        }
+        timer = setTimeout(function(){
+            func.call(this,...args);
+            timer = null;
+        },500)
+    }
+}
+// 滚动到某个区域
+function ScrollDiv(div,func,func_outer) {   
     let pageY = window.pageYOffset;
     let height = div.clientHeight;
     let div_top = div.offsetTop;
     // 如果当前滚动距离 - div距顶部距离 > 0 且 当前滚动距离-div距顶部距离小于div的高度 说明在当前div内
     if (pageY - div_top > 0 && pageY - div_top < height) {
-        //  进入了div区域
-        div.className='animation'
+        //  进入了div区域 
+        if(func) func(div);
+        console.log(div)
     } else{
         // 离开了div区域
-        div.className=''
-
+        if(func_outer) func_outer(div);
     }
 };
+// div们
+let containerFirst = document.querySelector('#containerFirst');
+let containerSecond = document.querySelector('#containerSecond');
+
+// 第一个div绑定事件
+let thro_scroll_func = function(){
+    containerSecond.style.filter = "grayscale(0%)";
+    containerSecond.style.color = "#000"; 
+} 
+let thro_scroll_outerFunc = function(){ 
+    containerSecond.style.filter = "grayscale(100%)";
+    containerSecond.style.color = "#fff";  
+}
+let thro_scroll = throttle(ScrollDiv,containerFirst,thro_scroll_func,thro_scroll_outerFunc) ;
+
+// 滚动事件绑定
 window.onscroll = function () {
-    divs.forEach(e => { 
-        ScrollDiv(e);
-    })
+    thro_scroll();
 }
